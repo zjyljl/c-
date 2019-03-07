051f890e45b002530dc72bb75164e5b32a73caaa
zjyljl <45060902+zjyljl@users.noreply.github.com>
2019-03-07 22:18:38 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,38 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2_9
+namespace _2_6
 {
     class Program
     {
+     static bool Isprime(int n)//判断整数n是否为素数
+        {
+            int i;
+            for (i = 2; i <=Math.Sqrt(n); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+                if (i > Math.Sqrt(n))
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
-            int []aa = new int[101];
-            aa[0] = aa[1] = 1;
-            aa[2] = 0;
-            int k = 2, tt = 0;
-            while (tt < 101)
+            int n;
+            string s;
+            Console.Write("输入一个数:");
+            s = Console.ReadLine();
+            n = int.Parse(s);
+            Console.Write(n + "的所有素数因子为:");
+            for(int i=2;i<n;++i)
             {
-                for (int i = 1; i < aa.Length; i++) //将不是素数的数筛出
-                {
-                    if (i % k == 0 && i != k)
-                        aa[i] = 1;//不符合要求的全部置为1
-                }
-
-                for (int i = 1; i < aa.Length; i++) //将筛选后的第一个数当做新的筛子
-                {
-                    if (i > k && aa[i] == 0)
-                    {
-                        k = i;
-                        break;
-                    }
-                }
-                tt++;
-            }
-
-            for (int i = 1; i < aa.Length; i++)
-                if (aa[i] == 0)
+                if (n % i == 0 && Isprime(i))
                     Console.Write(i + " ");
+            }
             Console.WriteLine();
         }
     }
