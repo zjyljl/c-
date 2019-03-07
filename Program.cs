620266d6b5ed0c0d5a3b9f9646068af9f89a7142
zjyljl <45060902+zjyljl@users.noreply.github.com>
2019-03-07 22:27:02 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,41 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2_6
+namespace _2_7
 {
     class Program
     {
-     static bool Isprime(int n)//判断整数n是否为素数
-        {
-            int i;
-            for (i = 2; i <=Math.Sqrt(n); i++)
-            {
-                if (n % i == 0)
-                {
-                    return false;
-                }
-                if (i > Math.Sqrt(n))
-                {
-                    return true;
-                }
-            }
-            return true;
-        }
-
         static void Main(string[] args)
         {
-            int n;
-            string s;
-            Console.Write("输入一个数:");
-            s = Console.ReadLine();
-            n = int.Parse(s);
-            Console.Write(n + "的所有素数因子为:");
-            for(int i=2;i<n;++i)
+            int[] arr = { 12, 3, 0, -5, 36, 12, 64, 35, 15};
+            Console.Write("数组元素为:");
+            int sum = 0, aver = 0, max = arr[0], min = arr[0];
+            foreach (int i in arr)
             {
-                if (n % i == 0 && Isprime(i))
-                    Console.Write(i + " ");
+                Console.Write(i + " ");
+                sum += i;
+                if (min > i) min = i;
+                if (max < i) max = i;
             }
-            Console.WriteLine();
+            aver = sum / arr.Length;
+            Console.WriteLine("最大值:" + max + "   " + "最小值:" + min + "    " + "平均值:" + aver + "  " + "总和:" + sum);
         }
     }
 }
