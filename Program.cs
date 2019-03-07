8069251fc8935067487f7fac7a075375c6ebc229
zjyljl <45060902+zjyljl@users.noreply.github.com>
2019-03-07 22:28:42 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,24 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2_7
+namespace _2_9
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = { 12, 3, 0, -5, 36, 12, 64, 35, 15};
-            Console.Write("数组元素为:");
-            int sum = 0, aver = 0, max = arr[0], min = arr[0];
-            foreach (int i in arr)
+            int []aa = new int[101];
+            aa[0] = aa[1] = 1;
+            aa[2] = 0;
+            int k = 2, tt = 0;
+            while (tt < 101)
             {
-                Console.Write(i + " ");
-                sum += i;
-                if (min > i) min = i;
-                if (max < i) max = i;
+                for (int i = 1; i < aa.Length; i++) //将不是素数的数筛出
+                {
+                    if (i % k == 0 && i != k)
+                        aa[i] = 1;//不符合要求的全部置为1
+                }
+
+                for (int i = 1; i < aa.Length; i++) //将筛选后的第一个数当做新的筛子
+                {
+                    if (i > k && aa[i] == 0)
+                    {
+                        k = i;
+                        break;
+                    }
+                }
+                tt++;
             }
-            aver = sum / arr.Length;
-            Console.WriteLine("最大值:" + max + "   " + "最小值:" + min + "    " + "平均值:" + aver + "  " + "总和:" + sum);
+
+            for (int i = 1; i < aa.Length; i++)
+                if (aa[i] == 0)
+                    Console.Write(i + " ");
+            Console.WriteLine();
         }
     }
 }
